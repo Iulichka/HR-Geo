87d452411ca39dc01325983553c202e60f77fa1c
nkvira <nkvira12@freeuni.edu.ge>
2015-06-05 22:44:24 +0400

    experience

---
+++ src/backClasses/Experience.java
@@ -0,0 +1,13 @@
+package backClasses;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Nodo
+ *
+ */
+public class Experience {
+	private String position, company;
+	private Date startDate, endDate;
+}
