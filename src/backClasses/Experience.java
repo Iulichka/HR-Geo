f63e4c442f1d26d664f37a6322dee7248363e0a0
nkvira <nkvira12@freeuni.edu.ge>
2015-06-06 00:19:36 +0400

    overallExperience and Experience

--- src/backClasses/Experience.java
+++ src/backClasses/Experience.java
@@ -12,13 +12,76 @@ public class Experience {
 	private Date startDate, endDate;
 	private boolean isCurrent;
 	
-	
-	public Experience(String pos, String comp, Date st, Date end, boolean cur) {
+	// if endDate = null considered as current
+	public Experience(String pos, String comp, Date st, Date end) {
 		position = pos;
 		company = comp;
 		startDate = st;
 		endDate = end;
+		isCurrent = (end == null);
+	}
+	
+	public String getCompName() {
+		return company;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public int getDuration() {
+		if (endDate == null) return new Date().getYear()-startDate.getYear();
+		return endDate.getYear() - startDate.getYear();
+	}
+	
+	public boolean isCurrent() {
+		return isCurrent;
+	}
+	
+	public void setPosition(String pos) {
+		position = pos;
+	}
+	
+	public void setCompany(String comp) {
+		company = comp;
+	}
+	
+	public void setStartDate(Date d) {
+		startDate = d;
+	}
+	
+	public void setEndDate(Date d) {
+		endDate = d;
+	}
+	
+	public void setCurrent(boolean cur) {
 		isCurrent = cur;
+	}
+	
+	@Override
+	public int hashCode() {
+		return startDate.hashCode() + company.hashCode()/position.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if((obj == null) || (obj.getClass() != this.getClass())) { 
+			return false; 
+		}
+		Experience exp = (Experience) obj;
+		if (exp.getCompName().equals(company) && exp.getStartDate().equals(startDate) &&
+				exp.getPosition().equals(position)) return true;
+		return false;
 	}
 	
 }
---
+++ src/backClasses/OverallExperience.java
@@ -0,0 +1,37 @@
+package backClasses;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class OverallExperience {
+	private Set<Experience> exps;
+	
+	public OverallExperience() {
+		exps = new HashSet<Experience>();
+	}
+	/**
+	 * 
+	 * @return sum of all years of experience
+	 */
+	public int getSumOfYears() {
+		int res = 0;
+		Iterator<Experience> it = exps.iterator();
+		while(it.hasNext()) {
+			res = res + it.next().getDuration();
+		}
+		return res;
+	}
+	
+	public void addExperience(Experience e) {
+		exps.add(e);
+	}
+	
+	public int getQuantity() {
+		return exps.size();
+	}
+	
+	public void removeExperience(Experience e) {
+		exps.remove(e);
+	}
+}
