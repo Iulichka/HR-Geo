b82093e904d34cbb6e4f2e0891a19f8ded5f28a0
LevanGabaidze <you@example.com>
2015-06-18 23:04:50 +0400

    adding category to Skill.java

--- src/backClasses/Skill.java
+++ src/backClasses/Skill.java
@@ -11,11 +11,20 @@ public class Skill {
 		 * 
 		 */
 		private int level;
+		private String skillCategory;
 		private int skillId;
-		public Skill(String name,int level,int id){
+		public Skill(String name,int level,int id,String skillCategory){
 			this.name=name;
 			this.level=level;
 			this.skillId=id;
+			this.skillCategory=skillCategory;
+		}
+		
+		public String getCategory(){
+			return skillCategory;
+		}
+		public void setCategoru(String category){
+			this.skillCategory=category;
 		}
 		
 		public int getLevel(){
