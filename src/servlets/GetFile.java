82f224425315000ccc3a937c0ce67c3ba2ffe8ae
nkvira <nkvira12@freeuni.edu.ge>
2015-06-23 21:29:54 +0400

    more generic servlet for getting files from byte array

---
+++ src/servlets/GetFile.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import backClasses.DataForComp;
+
+/**
+ * Servlet implementation class GetFile
+ * this servlet gets file attribute from  request and sends it back
+ * type parameter is file type like "text/html"
+ */
+@WebServlet("/GetFile")
+public class GetFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetFile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String fileType = request.getParameter("type");
+		response.setContentType(fileType);
+		byte [] file = (byte[]) request.getAttribute("file");
+		response.setContentLength(file.length);
+		OutputStream out = response.getOutputStream();
+		System.out.println(file.length);
+		out.write(file);
+	}
+
+}
