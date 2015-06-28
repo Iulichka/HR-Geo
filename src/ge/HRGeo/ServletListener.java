36ba3b848907a8f01720fb695c48cc10bad0e1a5
datoAbesadze <dabes11@freeuni.edu.ge>
2015-06-28 14:15:46 +0400

    servlet context listener

---
+++ src/ge/HRGeo/ServletListener.java
@@ -0,0 +1,20 @@
+package ge.HRGeo;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ServletListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
