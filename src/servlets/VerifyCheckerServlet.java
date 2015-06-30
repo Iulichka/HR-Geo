c17b7c5e147d5c3262d8e9ca39df227e9d1eaf34
Iulichka <itvel11@freeuni.edu.ge>
2015-06-30 14:37:45 +0400

    dd

---
+++ src/servlets/VerifyCheckerServlet.java
@@ -0,0 +1,43 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class VerifyCheckerServlet
+ */
+@WebServlet("/VerifyCheckerServlet")
+public class VerifyCheckerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VerifyCheckerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(!request.getParameter("code").equals(request.getSession().getAttribute("code"))){
+			request.getRequestDispatcher("illegalRegistr.jsp").forward(request, response);
+		}else{
+			request.getRequestDispatcher("Success.jsp").forward(request, response);
+		}
+	}
+
+}
